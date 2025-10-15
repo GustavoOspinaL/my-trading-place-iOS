221a5a46c728d651ce374b285a208e4a2013ca31
GustavoOspinaL <tavolopez.dev@gmail.com>
2025-10-14 22:31:55 -0500

    Initial Commit

---
+++ MyTradingPlace/MyTradingPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTradingPlaceApp.swift
+//  MyTradingPlace
+//
+//  Created by Tavo Lopez on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTradingPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyTradingPlaceTests/MyTradingPlaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyTradingPlaceTests.swift
+//  MyTradingPlaceTests
+//
+//  Created by Tavo Lopez on 14/10/25.
+//
+
+import Testing
+@testable import MyTradingPlace
+
+struct MyTradingPlaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
